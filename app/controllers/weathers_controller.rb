11ff7cb5147535d6780932be330e7db6f6c6d709
Jay Wengrow <jaywngrw@gmail.com>
2014-11-06 11:13:03 -0600

    initial commit

---
+++ app/controllers/weathers_controller.rb
@@ -0,0 +1,8 @@
+class WeathersController < ApplicationController
+  def index
+    response = Weather.lookup(12784296, Weather::Units::FARENHEIT)
+    @temp = response.condition.temp
+    @conditions = response.condition.text
+    @image = response.image.url
+  end
+end
